7a21d4fd86cab85341b56c7768fdd299db246ad0
Samus Mihld <SLMankey87@gmail.com>
2016-11-11 08:20:48 -0800

    Changes 1

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,8 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+function topleft() {
+  var namespace = "http://www.w3.org/2000/svg"
+  document.getElementById("square 1")
+  var circle 
+}
